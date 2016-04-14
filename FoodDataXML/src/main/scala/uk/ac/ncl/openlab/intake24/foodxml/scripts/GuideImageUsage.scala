5a865dd5b15f9a68647c0f78de311e5fa9e38298
Ivan Poliakov <ivan.poliakov@ncl.ac.uk>
2016-04-14 15:57:16 +0100

    Scripts

---
+++ FoodDataXML/src/main/scala/uk/ac/ncl/openlab/intake24/foodxml/scripts/GuideImageUsage.scala
@@ -0,0 +1,36 @@
+package uk.ac.ncl.openlab.intake24.foodxml.scripts
+
+import uk.ac.ncl.openlab.intake24.foodxml.FoodDef
+import scala.xml.XML
+import net.scran24.fooddef.PortionSizeMethodParameter
+import au.com.bytecode.opencsv.CSVWriter
+import java.io.File
+import java.io.FileWriter
+
+object GuideImageUsage extends App {
+
+  case class Reference(guideId: String, foodName: String, foodCode: String)
+
+  val dataDir = "/home/ivan/Projects/Intake24/intake24-data"
+
+  val foods = FoodDef.parseXml(XML.load(dataDir + "/foods.xml"))
+
+  val refs = foods.flatMap {
+    food =>
+      food.localData.portionSize.filter(_.method == "guide-image").map(m => Reference(m.parameters.find(_.name == "guide-image-id").get.value, food.englishDescription, food.code))
+  }
+   
+  val usages = refs.groupBy(_.guideId)
+  
+  val writer = new CSVWriter(new FileWriter(new File("/home/ivan/tmp/guide_image_usage.csv")))
+    
+  writer.writeNext(Array("Guide image ID", "Used by"))
+  
+  usages.keySet.toSeq.sorted.foreach {
+    code =>
+      val u = usages(code)
+      writer.writeNext(code +: u.map(_.foodCode).toArray)
+  }
+  
+  writer.close()
+}
