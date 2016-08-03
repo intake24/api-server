33136ef075c9477843402be9303ec2538cc53574
Ivan Poliakov <ivan.poliakov@ncl.ac.uk>
2016-08-03 12:33:01 +0100

    Fixed locale parameter being ignore for quick search function

--- FoodDataSQL/src/main/scala/uk/ac/ncl/openlab/intake24/foodsql/AdminFoodDataServiceSqlImpl.scala
+++ FoodDataSQL/src/main/scala/uk/ac/ncl/openlab/intake24/foodsql/AdminFoodDataServiceSqlImpl.scala
@@ -498,7 +498,7 @@ class AdminFoodDataServiceSqlImpl @Inject() (@Named("intake24_foods") val dataSo
         """|SELECT code, description, local_description, do_not_use
            |FROM foods LEFT JOIN foods_local ON foods.code = foods_local.food_code 
            |WHERE (lower(local_description) LIKE {pattern} OR lower(description) LIKE {pattern} OR lower(code) LIKE {pattern})
-           |AND foods_local.locale_id = {locale_id} 
+           |AND foods_local.locale_id = {locale_id}
            |ORDER BY local_description DESC
            |LIMIT 30""".stripMargin
 
