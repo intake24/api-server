c146840cbf2aca4b266850e8accedc531ff48b1b
Tim Osadchiy <t.osadchiy@ncl.ac.uk>
2017-02-09 09:53:45 +0000

    migration fix

--- FoodDataSQL/src/main/scala/uk/ac/ncl/openlab/intake24/foodsql/migrations/FeedbackDemographicScaleMigration.scala
+++ FoodDataSQL/src/main/scala/uk/ac/ncl/openlab/intake24/foodsql/migrations/FeedbackDemographicScaleMigration.scala
@@ -31,7 +31,7 @@ object FeedbackDemographicScaleMigration extends Migration {
          |    name character varying(500) NOT NULL
          |);
          |
-         |CREATE TABLE demographic_scale (
+         |CREATE TABLE demographic_group (
          |    id serial PRIMARY KEY,
          |    age int4range,
          |    height numrange,
@@ -39,9 +39,9 @@ object FeedbackDemographicScaleMigration extends Migration {
          |    sex sex_enum,
          |    physical_activity_level_id integer NOT NULL,
          |    nutrient_type_id integer NOT NULL,
-         |    CONSTRAINT demographic_scale_nutrient_type_fk FOREIGN KEY(physical_activity_level_id)
+         |    CONSTRAINT demographic_group_nutrient_type_fk FOREIGN KEY(physical_activity_level_id)
          |        REFERENCES level_of_physical_activity(id),
-         |    CONSTRAINT demographic_scale_physical_activity_level_fk FOREIGN KEY(nutrient_type_id)
+         |    CONSTRAINT demographic_group_physical_activity_level_fk FOREIGN KEY(nutrient_type_id)
          |        REFERENCES nutrient_types(id),
          |    CONSTRAINT factor_scale_age_positive CHECK (lower(age) >= 0),
          |    CONSTRAINT factor_scale_height_positive CHECK (lower(height) >= 0),
@@ -49,21 +49,21 @@ object FeedbackDemographicScaleMigration extends Migration {
          |    CONSTRAINT factor_scale_sex_check CHECK (sex IN ('m', 'f'))
          |);
          |
-         |COMMENT ON TABLE demographic_scale IS 'Table for storing demographic rule groups of nutrient ranges for giving participants feedback on their survey';
+         |COMMENT ON TABLE demographic_group IS 'Table for storing demographic rule groups of nutrient ranges for giving participants feedback on their survey';
          |
-         |CREATE TABLE demographic_scale_sector (
+         |CREATE TABLE demographic_group_scale_sector (
          |    id serial PRIMARY KEY,
-         |    demographic_scale_id integer NOT NULL,
+         |    demographic_group_id integer NOT NULL,
          |    name character varying(500) NOT NULL,
          |    description character varying(50000),
          |    sentiment sentiment_enum NOT NULL DEFAULT 'neutral',
          |    range numrange NOT NULL,
-         |    CONSTRAINT demographic_scale_sector_demographic_scale_fk FOREIGN KEY(demographic_scale_id)
-         |        REFERENCES demographic_scale(id) ON UPDATE CASCADE ON DELETE CASCADE,
-         |    CONSTRAINT demographic_scale_sector_no_overlap EXCLUDE USING gist (demographic_scale_id WITH =, range WITH &&)
+         |    CONSTRAINT demographic_group_scale_sector_demographic_group_fk FOREIGN KEY(demographic_group_id)
+         |        REFERENCES demographic_group(id) ON UPDATE CASCADE ON DELETE CASCADE,
+         |    CONSTRAINT demographic_group_scale_sector_no_overlap EXCLUDE USING gist (demographic_group_id WITH =, range WITH &&)
          |);
          |
-         |COMMENT ON TABLE demographic_scale_sector IS 'Table for storing demographic rules of nutrient ranges for giving participants feedback on their survey';
+         |COMMENT ON TABLE demographic_group_scale_sector IS 'Table for storing demographic rules of nutrient ranges for giving participants feedback on their survey';
          |
       """.stripMargin
 
@@ -75,8 +75,8 @@ object FeedbackDemographicScaleMigration extends Migration {
   def unapply(logger: Logger)(implicit connection: Connection): Either[MigrationFailed, Unit] = {
 
     val sqlQuery =
-      """|DROP TABLE demographic_scale_sector;
-         |DROP TABLE demographic_scale;
+      """|DROP TABLE demographic_group_scale_sector;
+         |DROP TABLE demographic_group;
          |DROP TABLE level_of_physical_activity;
          |DROP TYPE sentiment_enum;
          |DROP TYPE sex_enum;
