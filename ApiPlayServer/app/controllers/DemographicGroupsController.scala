9294fc48d086874dd1d373f35e27a0ea87066310
Tim Osadchiy <t.osadchiy@ncl.ac.uk>
2017-02-15 15:21:15 +0000

    api

--- ApiPlayServer/app/controllers/DemographicGroupsController.scala
+++ ApiPlayServer/app/controllers/DemographicGroupsController.scala
@@ -5,12 +5,14 @@ import parsers.UpickleUtil
 import play.mvc.Controller
 import security.{DeadboltActionsAdapter, Roles}
 import uk.ac.ncl.openlab.intake24.services.fooddb.demographicgroups._
+import uk.ac.ncl.openlab.intake24.services.fooddb.errors._
 import upickle.Js
 
 import scala.collection.immutable.NumericRange
 import scala.concurrent.Future
 
 import play.api.libs.concurrent.Execution.Implicits._
+
 /**
   * Created by Tim Osadchiy on 09/02/2017.
   */
@@ -18,15 +20,51 @@ class DemographicGroupsController @Inject()(dgService: DemographicGroupsService,
                                             deadbolt: DeadboltActionsAdapter)
   extends Controller with ImageOrDatabaseServiceErrorHandler with UpickleUtil {
 
-  implicit private val rangeWriter = upickle.default.Writer[NumericRange[Double]] {
-    r =>
-      Js.Obj(("start", Js.Num(r.start)), ("end", Js.Num(r.end)))
-
-  }
-
   def list() = deadbolt.restrictAccess(Roles.superuser) {
     Future {
       translateDatabaseResult(dgService.list())
+    }
+  }
+
+  def createDemographicGroup() = deadbolt.restrictAccess(Roles.superuser)(upickleBodyParser[DemographicGroupRecordIn]) {
+    request => Future {
+      translateDatabaseResult(dgService.createDemographicGroup(request.body))
+    }
+  }
+
+  def patchDemographicGroup(id: Int) = deadbolt.restrictAccess(Roles.superuser)(upickleBodyParser[DemographicGroupRecordIn]) {
+    request => Future {
+      translateDatabaseResult(dgService.patchDemographicGroup(id, request.body))
+    }
+  }
+
+  def getDemographicGroup(id: Int) = deadbolt.restrictAccess(Roles.superuser) {
+    request => Future {
+      translateDatabaseResult(dgService.getDemographicGroup(id))
+    }
+  }
+
+  def deleteDemographicGroup(id: Int) = deadbolt.restrictAccess(Roles.superuser) {
+    request => Future {
+      translateDatabaseResult(dgService.deleteDemographicGroup(id))
+    }
+  }
+
+  def createDemographicGroupScaleSector(demographicGroupId: Int) = deadbolt.restrictAccess(Roles.superuser)(upickleBodyParser[DemographicScaleSectorIn]) {
+    request => Future {
+      translateDatabaseResult(dgService.createDemographicScaleSector(demographicGroupId, request.body))
+    }
+  }
+
+  def patchDemographicGroupScaleSector(id: Int) = deadbolt.restrictAccess(Roles.superuser)(upickleBodyParser[DemographicScaleSectorIn]) {
+    request => Future {
+      translateDatabaseResult(dgService.patchDemographicScaleSector(id, request.body))
+    }
+  }
+
+  def deleteDemographicGroupScaleSector(id: Int) = deadbolt.restrictAccess(Roles.superuser) {
+    request => Future {
+      translateDatabaseResult(dgService.deleteDemographicScaleSector(id))
     }
   }
 
--- FoodDataServices/src/main/scala/uk/ac/ncl/openlab/intake24/services/fooddb/demographicgroups/DemographicGroupsService.scala
+++ FoodDataServices/src/main/scala/uk/ac/ncl/openlab/intake24/services/fooddb/demographicgroups/DemographicGroupsService.scala
@@ -4,27 +4,60 @@ import uk.ac.ncl.openlab.intake24.services.fooddb.errors._
 
 import scala.collection.immutable.NumericRange
 
-case class DoubleRange(start: Double, end: Double)
+abstract class NumRange[T] {
+  val start: T
+  val end: T
+}
 
-case class IntRange(start: Int, end: Int)
+case class DoubleRange(override val start: Double, override val end: Double) extends NumRange[Double]
 
-case class DemographicScaleSectorRecord(id: Long,
-                                        name: String,
-                                        description: Option[String],
-                                        sentiment: String,
-                                        range: NumericRange[Double])
+case class IntRange(override val start: Int, override val end: Int) extends NumRange[Int]
 
-case class DemographicGroupRecord(id: Long,
-                                  sex: Option[String],
-                                  age: Option[IntRange],
-                                  height: Option[DoubleRange],
-                                  weight: Option[DoubleRange],
-                                  physicalLevelId: Option[Long],
-                                  scaleSectors: Seq[DemographicScaleSectorRecord]
-                                 )
+case class DemographicScaleSectorOut(id: Long,
+                                     name: String,
+                                     description: Option[String],
+                                     sentiment: String,
+                                     range: DoubleRange)
+
+case class DemographicScaleSectorIn(name: String,
+                                    description: Option[String],
+                                    sentiment: String,
+                                    range: DoubleRange)
+
+case class DemographicGroupRecordOut(id: Long,
+                                     sex: Option[String],
+                                     age: Option[IntRange],
+                                     height: Option[DoubleRange],
+                                     weight: Option[DoubleRange],
+                                     physicalLevelId: Option[Long],
+                                     nutrientLevelId: Long,
+                                     scaleSectors: Seq[DemographicScaleSectorOut]
+                                    )
+
+case class DemographicGroupRecordIn(sex: Option[String],
+                                    age: Option[IntRange],
+                                    height: Option[DoubleRange],
+                                    weight: Option[DoubleRange],
+                                    physicalLevelId: Option[Long],
+                                    nutrientLevelId: Long
+                                   )
 
 trait DemographicGroupsService {
 
-  def list(): Either[UnexpectedDatabaseError, Seq[DemographicGroupRecord]]
+  def list(): Either[UnexpectedDatabaseError, Seq[DemographicGroupRecordOut]]
+
+  def createDemographicGroup(demographicRecord: DemographicGroupRecordIn): Either[ConstraintError, DemographicGroupRecordOut]
+
+  def patchDemographicGroup(id: Int, demographicRecord: DemographicGroupRecordIn): Either[UpdateError, DemographicGroupRecordOut]
+
+  def getDemographicGroup(id: Int): Either[LookupError, DemographicGroupRecordOut]
+
+  def deleteDemographicGroup(id: Int): Either[UnexpectedDatabaseError, Unit]
+
+  def createDemographicScaleSector(demographicGroupId: Int, demographicRecord: DemographicScaleSectorIn): Either[UpdateError, DemographicScaleSectorOut]
+
+  def patchDemographicScaleSector(id: Int, demographicRecord: DemographicScaleSectorIn): Either[UpdateError, DemographicScaleSectorOut]
+
+  def deleteDemographicScaleSector(id: Int): Either[UnexpectedDatabaseError, Unit]
 
 }
