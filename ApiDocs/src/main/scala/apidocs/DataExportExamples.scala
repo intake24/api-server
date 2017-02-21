c39e6c0e58b940f30e14d75e1e28babb61920fef
Ivan Poliakov <ivan.poliakov@ncl.ac.uk>
2017-02-21 15:53:54 +0000

    Data export API docs

---
+++ ApiDocs/src/main/scala/apidocs/DataExportExamples.scala
@@ -0,0 +1,23 @@
+package apidocs
+
+import java.time.Clock
+import java.time.temporal.{ChronoField, TemporalField}
+import java.util.UUID
+
+import parsers.UpickleUtil
+import uk.ac.ncl.openlab.intake24.api.shared._
+import uk.ac.ncl.openlab.intake24.services.systemdb.admin.{PublicUserRecord, PublicUserRecordWithPermissions}
+import uk.ac.ncl.openlab.intake24.surveydata._
+
+object DataExportExamples extends UpickleUtil {
+
+  import JSONPrettyPrinter._
+
+  val surveySubmissions = asPrettyJSON(Seq[NutrientMappedSubmission](
+    NutrientMappedSubmission(UUID.fromString("2e90f16a-c8b7-4f68-9a3c-44e4b92ee807"), "user1", Map("userCustomField1" -> "123"), Map("surveyCustomField1" -> "456"),
+      Clock.systemUTC().instant().minusSeconds(600).`with`(ChronoField.NANO_OF_SECOND, 0), Clock.systemUTC().instant().`with`(ChronoField.NANO_OF_SECOND, 0), Seq(NutrientMappedMeal("Breakfast", MealTime(8, 0), Map("mealCustomField1" -> "abc"), Seq(NutrientMappedFood(
+        "FOOD001", "Example food", Some("Exemple alimentaire"), "good food", "NDNS", "1234", false, CompletedPortionSize("as-served", Map("servingWeight" -> "100", "leftoversWeight" -> "50")), true,
+        10, "Some brand", Map(1 -> 5, 2 -> 10, 3 -> 14), Map("foodCustomField" -> "asdf"))
+      ))))
+  ))
+}
