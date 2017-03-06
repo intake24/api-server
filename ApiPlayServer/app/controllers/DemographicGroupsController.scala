3e3b619570bfbf2d5467b2b7e25a8f3e087ef3b2
Tim Osadchiy <t.osadchiy@ncl.ac.uk>
2017-03-06 15:10:17 +0000

    henry coefficients

--- ApiPlayServer/app/controllers/DemographicGroupsController.scala
+++ ApiPlayServer/app/controllers/DemographicGroupsController.scala
@@ -2,16 +2,13 @@ package controllers
 
 import com.google.inject.Inject
 import parsers.UpickleUtil
+import play.api.http.ContentTypes
 import play.mvc.Controller
 import security.{DeadboltActionsAdapter, Roles}
 import uk.ac.ncl.openlab.intake24.services.fooddb.demographicgroups._
-import uk.ac.ncl.openlab.intake24.errors._
-import upickle.Js
-
-import scala.collection.immutable.NumericRange
 import scala.concurrent.Future
-
 import play.api.libs.concurrent.Execution.Implicits._
+import upickle.default.write
 
 /**
   * Created by Tim Osadchiy on 09/02/2017.
@@ -63,9 +60,35 @@ class DemographicGroupsController @Inject()(dgService: DemographicGroupsService,
   }
 
   def deleteDemographicGroupScaleSector(id: Int) = deadbolt.restrictAccess(Roles.superuser) {
-    request => Future {
+    _ => Future {
       translateDatabaseResult(dgService.deleteDemographicScaleSector(id))
     }
   }
 
+  def getHenryCoefficients() = deadbolt.restrictAccess(Roles.superuser, Roles.survey_admin, Roles.survey_respondent) {
+    _ => Future {
+
+      val result = Seq(
+        HenryCoefficients("m", IntRange(0 ,3), 28.2, 859, -371),
+        HenryCoefficients("m", IntRange(3 ,10), 15.1, 313, 306),
+        HenryCoefficients("m", IntRange(10 ,18), 15.6, 266, 299),
+        HenryCoefficients("m", IntRange(18 ,30), 14.4, 313, 113),
+        HenryCoefficients("m", IntRange(30 ,60), 11.4, 541, -137),
+        HenryCoefficients("m", IntRange(60, Int.MaxValue), 11.4, 541, -256),
+
+        HenryCoefficients("f", IntRange(0, 3), 30.4, 703, -287),
+        HenryCoefficients("f", IntRange(3, 10), 15.9, 210, 349),
+        HenryCoefficients("f", IntRange(10, 18), 9.4, 249, 462),
+        HenryCoefficients("f", IntRange(18, 30), 10.4, 615, -282),
+        HenryCoefficients("f", IntRange(30, 60), 8.18, 502, -11.6),
+        HenryCoefficients("f", IntRange(60, Int.MaxValue), 8.52, 421, 10.7)
+      )
+
+      Ok(write(result)).as(ContentTypes.JSON)
+    }
+  }
+
 }
+
+case class HenryCoefficients(sex: String, ageRange: IntRange, weightCoefficient: Double,
+                             heightCoefficient: Double, constant: Double)
