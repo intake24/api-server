f32bbf69ac29ba86cb0b066726179814fd2c0e30
Tim Osadchiy <t.osadchiy@ncl.ac.uk>
2017-10-04 10:50:04 +0100

    pairwise associations service impl

--- SystemDataSQL/src/main/scala/uk/ac/ncl/openlab/intake24/systemsql/pairwiseAssociations/PairwiseAssociationsDataServiceImpl.scala
+++ SystemDataSQL/src/main/scala/uk/ac/ncl/openlab/intake24/systemsql/pairwiseAssociations/PairwiseAssociationsDataServiceImpl.scala
@@ -6,7 +6,7 @@ import javax.sql.DataSource
 import uk.ac.ncl.openlab.intake24.pairwiseAssociationRules.{PairwiseAssociationRules, PairwiseAssociationRulesConstructorParams}
 import uk.ac.ncl.openlab.intake24.services.systemdb.pairwiseAssociations.PairwiseAssociationsDataService
 import uk.ac.ncl.openlab.intake24.sql.SqlDataService
-import anorm.{Macro, SQL}
+import anorm.{BatchSql, Macro, NamedParameter, SQL}
 import uk.ac.ncl.openlab.intake24.errors.{LookupError, RecordNotFound, UnexpectedDatabaseError}
 
 /**
@@ -14,11 +14,35 @@ import uk.ac.ncl.openlab.intake24.errors.{LookupError, RecordNotFound, Unexpecte
   */
 class PairwiseAssociationsDataServiceImpl @Inject()(@Named("intake24_system") val dataSource: DataSource) extends PairwiseAssociationsDataService with SqlDataService {
 
-  private val coOccurrenceQuery = "SELECT * FROM pairwise_associations_co_occurrences;"
+  private val coOccurrenceSelectSql = "SELECT * FROM pairwise_associations_co_occurrences;"
 
-  private val occurrencesQuery = "SELECT * FROM pairwise_associations_occurrences;"
+  private val occurrencesSelectSql = "SELECT * FROM pairwise_associations_occurrences;"
 
-  private val transactionsCountQuery = "SELECT * FROM pairwise_associations_transactions_count;"
+  private val transactionsCountSelectSql = "SELECT * FROM pairwise_associations_transactions_count;"
+
+  private val coOccurrenceUpdateSql =
+    """
+      |INSERT INTO pairwise_associations_co_occurrences (locale, antecedent_food_code, consequent_food_code, occurrences)
+      |VALUES ({locale}, {antecedent_food_code}, {consequent_food_code}, {occurrences})
+      |ON CONFLICT (locale, antecedent_food_code, consequent_food_code)
+      |  DO UPDATE SET occurrences = pairwise_associations_co_occurrences.occurrences + {occurrences};
+    """.stripMargin
+
+  private val occurrenceUpdateSql =
+    """
+      |INSERT INTO pairwise_associations_occurrences (locale, food_code, occurrences)
+      |VALUES ({locale}, {food_code}, {occurrences})
+      |ON CONFLICT (locale, food_code)
+      |  DO UPDATE SET occurrences = pairwise_associations_occurrences.occurrences + {occurrences};
+    """.stripMargin
+
+  private val transactionCountUpdateSql =
+    """
+      |INSERT INTO pairwise_associations_transactions_count (locale, transactions_count)
+      |VALUES ({locale}, {transactions_count})
+      |ON CONFLICT (locale)
+      |  DO UPDATE SET transactions_count = pairwise_associations_transactions_count.transactions_count + {transactions_count};
+    """.stripMargin
 
   private case class CoOccurrenceRow(locale: String, antecedent_food_code: String, consequent_food_code: String, occurrences: Int)
 
@@ -44,11 +68,30 @@ class PairwiseAssociationsDataServiceImpl @Inject()(@Named("intake24_system") va
     ) yield associationRules
   }
 
-  override def addTransactions(locale: String, transactions: Seq[Seq[String]]): Unit = ???
+  override def addTransactions(locale: String, transactions: Seq[Seq[String]]): Unit = {
+    val p = PairwiseAssociationRules(None)
+    p.addTransactions(transactions)
+
+    val params = p.getParams()
+    val coOccurrenceUpdateParams = params.coOccurrences.flatMap { antFoodNode =>
+      antFoodNode._2.map { consFoodNode =>
+        Seq[NamedParameter]('locale -> locale, 'antecedent_food_code -> antFoodNode._1,
+          'consequent_food_code -> consFoodNode._1, 'occurrences -> consFoodNode._2)
+      }
+    }.toSeq
+    val occurrenceUpdateParams = params.occurrences.map { node =>
+      Seq[NamedParameter]('locale -> locale, 'food_code -> node._1, 'occurrences -> node._2)
+    }.toSeq
+    val transactionCountsUpdateParams = Seq[NamedParameter]('locale -> locale, 'transactions_count -> params.numberOfTransactions)
+
+    BatchSql(coOccurrenceUpdateSql, coOccurrenceUpdateParams.head, coOccurrenceUpdateParams.tail: _*).execute()
+    BatchSql(occurrenceUpdateSql, occurrenceUpdateParams.head, occurrenceUpdateParams.tail: _*).execute()
+    SQL(transactionCountUpdateSql).on(transactionCountsUpdateParams: _*).execute()
+  }
 
   private def getCoOccurrenceMap(): Either[UnexpectedDatabaseError, Map[String, Map[String, Map[String, Int]]]] = tryWithConnection {
     implicit conn =>
-      val mp = SQL(coOccurrenceQuery).executeQuery().as(Macro.namedParser[CoOccurrenceRow].*).groupBy(_.locale).map { localeNode =>
+      val mp = SQL(coOccurrenceSelectSql).executeQuery().as(Macro.namedParser[CoOccurrenceRow].*).groupBy(_.locale).map { localeNode =>
         localeNode._1 -> localeNode._2.groupBy(_.antecedent_food_code).map { antNode =>
           antNode._1 -> antNode._2.groupBy(_.consequent_food_code).map { consNode =>
             consNode._1 -> consNode._2.head.occurrences
@@ -60,7 +103,7 @@ class PairwiseAssociationsDataServiceImpl @Inject()(@Named("intake24_system") va
 
   private def getOccurrenceMap(): Either[UnexpectedDatabaseError, Map[String, Map[String, Int]]] = tryWithConnection {
     implicit conn =>
-      val mp = SQL(occurrencesQuery).executeQuery().as(Macro.namedParser[OccurrenceRow].*).groupBy(_.locale).map { n =>
+      val mp = SQL(occurrencesSelectSql).executeQuery().as(Macro.namedParser[OccurrenceRow].*).groupBy(_.locale).map { n =>
         n._1 -> n._2.groupBy(_.food_code).map { fn => fn._1 -> fn._2.head.occurrences }
       }
       Right(mp)
@@ -68,7 +111,7 @@ class PairwiseAssociationsDataServiceImpl @Inject()(@Named("intake24_system") va
 
   private def getTransactionCounts(): Either[UnexpectedDatabaseError, Map[String, Int]] = tryWithConnection {
     implicit conn =>
-      val mp = SQL(transactionsCountQuery).executeQuery().as(Macro.namedParser[TransactionCountRow].*).groupBy(_.locale).map { n =>
+      val mp = SQL(transactionsCountSelectSql).executeQuery().as(Macro.namedParser[TransactionCountRow].*).groupBy(_.locale).map { n =>
         n._1 -> n._2.head.transactions_count
       }
       Right(mp)
