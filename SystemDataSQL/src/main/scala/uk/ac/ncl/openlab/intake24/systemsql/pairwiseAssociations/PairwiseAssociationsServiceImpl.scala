4ca8b63aeb5fc0197e3bb05408c64980112fe77e
Tim Osadchiy <t.osadchiy@ncl.ac.uk>
2017-10-04 20:11:32 +0100

    pairwise service implementation start

---
+++ SystemDataSQL/src/main/scala/uk/ac/ncl/openlab/intake24/systemsql/pairwiseAssociations/PairwiseAssociationsServiceImpl.scala
@@ -0,0 +1,8 @@
+package uk.ac.ncl.openlab.intake24.systemsql.pairwiseAssociations
+
+/**
+  * Created by Tim Osadchiy on 04/10/2017.
+  */
+class PairwiseAssociationsServiceImpl {
+
+}
---
+++ SystemDataServices/src/main/scala/uk/ac/ncl/openlab/intake24/services/systemdb/pairwiseAssociations/PairwiseAssociationsService.scala
@@ -0,0 +1,8 @@
+package uk.ac.ncl.openlab.intake24.services.systemdb.pairwiseAssociations
+
+/**
+  * Created by Tim Osadchiy on 04/10/2017.
+  */
+trait PairwiseAssociationsService {
+
+}
