af78d8b8af17e58b0c861c555d8c2d15d1ea1683
Ivan Poliakov <ivan.poliakov@ncl.ac.uk>
2018-01-31 10:55:08 +0000

    Fix bad refactoring

--- DatabaseTools/src/main/scala/uk/ac/ncl/openlab/intake24/sql/tools/food/nutrients/UsdaImport.scala
+++ DatabaseTools/src/main/scala/uk/ac/ncl/openlab/intake24/sql/tools/food/nutrients/UsdaImport.scala
@@ -2,4 +2,4 @@ package uk.ac.ncl.openlab.intake24.sql.tools.food.nutrients
 
 import uk.ac.ncl.openlab.intake24.nutrientsndns.{CsvNutrientTableMapping, LegacyNutrientTables}
 
-object UsdaImport extends CsvNutrientTableImport("USDA", "USDA Food Composition Database", CsvNutrientTableMapping(1, 0, 1, None, AusnutMapping.USDA))
+object UsdaImport extends CsvNutrientTableImport("USDA", "USDA Food Composition Database", CsvNutrientTableMapping(1, 0, 1, None, ArabicCompositionTableMappings.USDA))
