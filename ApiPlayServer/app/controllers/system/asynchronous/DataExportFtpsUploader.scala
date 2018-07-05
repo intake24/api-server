d3938a363a2aa09863161dbb42302990885cc9eb
Ivan Poliakov <ivan.poliakov@ncl.ac.uk>
2018-07-05 11:07:16 +0100

    Throw exceptions on FTP login errors

--- ApiPlayServer/app/controllers/system/asynchronous/DataExportFtpsUploader.scala
+++ ApiPlayServer/app/controllers/system/asynchronous/DataExportFtpsUploader.scala
@@ -4,30 +4,41 @@ import java.io.{File, FileInputStream, IOException}
 import java.net.SocketException
 import javax.inject.Singleton
 
-import org.apache.commons.net.ftp.FTPSClient
+import org.apache.commons.net.ftp.{FTPReply, FTPSClient}
 import org.apache.commons.net.util.TrustManagerUtils
 
 case class FTPSConfig(host: String, port: Int, userName: String, password: String, pathPrefix: String)
 
 @Singleton
 class DataExportFtpsUploader {
+
+  private def throwOnError(ftpsClient: FTPSClient)(block: => Boolean): Unit = if (!block) {
+    throw new IOException(ftpsClient.getReplyString)
+  }
+
   def upload(file: File, remoteName: String, config: FTPSConfig): Either[Throwable, Unit] = {
+    val ftpsClient = new FTPSClient()
     try {
-      val ftpsClient = new FTPSClient()
-
       ftpsClient.setTrustManager(TrustManagerUtils.getValidateServerCertificateTrustManager)
-
       ftpsClient.connect(config.host)
       ftpsClient.enterLocalPassiveMode()
-      ftpsClient.login(config.userName, config.password)
-      ftpsClient.storeFile(config.pathPrefix + "/" + remoteName, new FileInputStream(file))
+
+      throwOnError(ftpsClient) {
+        ftpsClient.login(config.userName, config.password)
+      }
+
+      throwOnError(ftpsClient) {
+        ftpsClient.storeFile(config.pathPrefix + "/" + remoteName, new FileInputStream(file))
+      }
+
       ftpsClient.logout()
-      ftpsClient.disconnect()
 
       Right(())
     } catch {
       case se: SocketException => Left(se)
       case ioe: IOException => Left(ioe)
+    } finally {
+      ftpsClient.disconnect()
     }
   }
 }
