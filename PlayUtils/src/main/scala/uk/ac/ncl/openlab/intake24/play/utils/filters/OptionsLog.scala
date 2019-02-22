1840520b3a411688d1e6d673426020101354fb9d
Ivan Poliakov <ivan.poliakov@ncl.ac.uk>
2019-02-22 16:27:42 +0000

    Request logger to debug CORS issues

---
+++ PlayUtils/src/main/scala/uk/ac/ncl/openlab/intake24/play/utils/filters/OptionsLog.scala
@@ -0,0 +1,28 @@
+package uk.ac.ncl.openlab.intake24.play.utils.filters
+
+import akka.stream.Materializer
+import javax.inject.Inject
+import org.slf4j.LoggerFactory
+import play.api.http.HttpVerbs
+import play.api.mvc.{Filter, RequestHeader, Result}
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class OptionsLog @Inject()(implicit val mat: Materializer, ec: ExecutionContext) extends Filter {
+
+  val logger = LoggerFactory.getLogger(classOf[OptionsLog])
+
+  def apply(nextFilter: RequestHeader => Future[Result])
+           (requestHeader: RequestHeader): Future[Result] = {
+
+    if (requestHeader.method == HttpVerbs.OPTIONS) {
+      logger.debug(s"Request: requestHeader.toString()")
+      requestHeader.headers.headers.foreach {
+        case (header, value) =>
+          logger.debug(s"  $header: $value")
+      }
+    }
+
+    nextFilter(requestHeader)
+  }
+}
