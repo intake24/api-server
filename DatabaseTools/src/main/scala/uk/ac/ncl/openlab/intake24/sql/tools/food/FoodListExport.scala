413950890deb1d83dbcc2085aabf9890064f0f97
Lukas Hroch <lukas.hroch@mrc-epid.cam.ac.uk>
2019-10-01 15:53:24 +0100

    few more columns to FoodListExport

--- DatabaseTools/src/main/scala/uk/ac/ncl/openlab/intake24/sql/tools/food/FoodListExport.scala
+++ DatabaseTools/src/main/scala/uk/ac/ncl/openlab/intake24/sql/tools/food/FoodListExport.scala
@@ -32,7 +32,10 @@ object FoodListExport extends App with DatabaseConnection with WarningMessage {
 
   val csvWriter = new CSVWriter(new FileWriter(new File(options.outputFile())))
 
-  csvWriter.writeNext(Array("Intake24 code", "English description", "Local description", "Food composition table", "Food composition record ID", "Portion size estimation methods", "Categories"))
+  csvWriter.writeNext(Array("Intake24 code", "English description", "Local description",
+    "Food composition table", "Food composition record ID",
+    "Ready Meal Option", "Same As Before Option", "Reasonable Amount", "Use In Recipes",
+    "Associated Food / Category", "Brand Names", "Portion size estimation methods", "Categories"))
 
   indexService.indexableFoods(options.locale()) match {
     case Right(foods) =>
@@ -50,11 +53,28 @@ object FoodListExport extends App with DatabaseConnection with WarningMessage {
 
               val psm = food.local.portionSize.map {
                 psm =>
-                  (Seq(s"Method: ${psm.method}", s"conversion: ${psm.conversionFactor}") ++ psm.parameters.map( p => s"${p.name}: ${p.value}")).mkString(", ")
+                  (Seq(s"Method: ${psm.method}", s"conversion: ${psm.conversionFactor}") ++ psm.parameters.map(p => s"${p.name}: ${p.value}")).mkString(", ")
               }.mkString("\n")
 
+              val readyMealOption = food.main.attributes.readyMealOption.getOrElse("Inherited").toString
+              val sameAsBeforeOption = food.main.attributes.sameAsBeforeOption.getOrElse("Inherited").toString
+              val reasonableAmount = food.main.attributes.reasonableAmount.getOrElse("Inherited").toString
+              val useInRecipes = food.main.attributes.useInRecipes.getOrElse("Inherited") match {
+                case 0 => "Anywhere"
+                case 1 => "RegularFoodsOnly"
+                case 2 => "RecipesOnly"
+                case _ => "Inherited"
+              }
+
+              val assocFoods = food.local.associatedFoods.map(_.toAssociatedFood).map(food => food.foodOrCategoryCode.merge).mkString(", ")
+              val brandNames = food.local.brandNames.mkString(", ")
+
               if (food.allowedInLocale(options.locale())) {
-                val row = Array(food.main.code, food.main.englishDescription, food.local.localDescription.getOrElse("N/A")) ++ Array(tableIds.mkString("\n"), recordIds.mkString("\n"), psm) ++ categories
+                val row =
+                  Array(food.main.code, food.main.englishDescription, food.local.localDescription.getOrElse("N/A")) ++
+                    Array(tableIds.mkString("\n"), recordIds.mkString("\n")) ++
+                    Array(readyMealOption, sameAsBeforeOption, reasonableAmount, useInRecipes,
+                      assocFoods, brandNames, psm) ++ categories
                 csvWriter.writeNext(row)
               }
 
