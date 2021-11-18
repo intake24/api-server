9ffba854db2a88de6c7fe2920d68f10704005ffd
Ivan Poliakov <ivpolyakov@gmail.com>
2021-11-18 12:24:55 +0000

    Fix create users API rejecting survey support roles

--- SystemDataServices/src/main/scala/uk/ac/ncl/openlab/intake24/services/systemdb/Roles.scala
+++ SystemDataServices/src/main/scala/uk/ac/ncl/openlab/intake24/services/systemdb/Roles.scala
@@ -17,12 +17,13 @@ object Roles {
 
   val staffSuffix = "/staff"
 
-  val foodDatabaseMaintainerPrefix = "fdbm/"
+  val supportSuffix = "/support"
 
+  val foodDatabaseMaintainerPrefix = "fdbm/"
 
   def surveyStaff(surveyId: String) = s"$surveyId$staffSuffix"
 
-  def surveySupport(surveyId: String) = s"$surveyId/support"
+  def surveySupport(surveyId: String) = s"$surveyId$supportSuffix"
 
   def surveyRespondent(surveyId: String) = s"$surveyId$respondentSuffix"
 
@@ -32,9 +33,12 @@ object Roles {
 
   def isSurveyStaff(role: String) = role.endsWith(staffSuffix) && role.length > staffSuffix.length
 
+  def isSurveySupport(role: String) = role.endsWith(supportSuffix) && role.length > supportSuffix.length
+
   def isSurveyRespondent(role: String) = role.endsWith(respondentSuffix) && role.length > respondentSuffix.length
 
   def isFoodDatabaseMaintainer(role: String) = role.startsWith(foodDatabaseMaintainerPrefix) && role.length > foodDatabaseMaintainerPrefix.length
 
-  def isValidRole(role: String): Boolean = isAdminRole(role) || isSurveyStaff(role) || isSurveyRespondent(role) || isFoodDatabaseMaintainer(role)
+  def isValidRole(role: String): Boolean =
+    isAdminRole(role) || isSurveyStaff(role) || isSurveySupport(role) || isSurveyRespondent(role) || isFoodDatabaseMaintainer(role)
 }
